3f818ff153f79ad1b2a00d6e74229d017388d3dd
Kristen Peck <kpeck71@gmail.com>
2017-12-09 03:02:24 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -8,6 +8,7 @@ class PigLatinizer
     if vowels.include?(word[0].downcase)
       word + "way"
     elsif pairs.include?(word[0,1].downcase)
+      word[1..0] + word[0,1] + "ay"
     else
       word[1..-1] + word[0] + "ay"
     end
