e0a1522b847cfc4c8129739dbf825643d0e9e798
Kristen Peck <kpeck71@gmail.com>
2017-12-09 02:45:06 +0000

    automatically backed up by learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,25 @@
+class PigLatinizer
+
+  def piglatinize(word)
+  skip = ["i", "me", "to", "too", "a", "an", "in", "and", "on"]
+  vowels = ["a", "e", "i", "o", "u"]
+
+    if skip.include?(word)
+      word
+    elsif vowels.include? word[0]
+      word << "ay"
+    else
+      consonants = ""
+      while !vowels.include?(word[0])
+        consonants << word[0]
+        word = word.split("")[1..-1].join
+      end
+      word + consonants + 'ay'
+    end
+  end
+
+  def to_pig_latin(string)
+    string.split.collect{|s| piglatinize(s)}.join(" ")
+  end
+
+end
