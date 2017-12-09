689c32c109c032b3c61c1c098055eccd5321b376
Kristen Peck <kpeck71@gmail.com>
2017-12-09 02:49:44 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,20 +4,13 @@ class PigLatinizer
   arr = word.split("").downcase
   size = word.size
   vowels = ["a", "e", "i", "o", "u"]
-
-    if skip.include?(word)
-      word
-    elsif vowels.include? word[0]
-      word << "ay"
-    else
-      consonants = ""
-      while !vowels.include?(word[0])
-        consonants << word[0]
-        word = word.split("")[1..-1].join
+  i = word_arr.index{|c| vowels.include?(c)}
+      if i == 0
+        word + "way"
+      else
+        word[i,size]+word[0,i]+"ay"
       end
-      word + consonants + 'ay'
     end
-  end
 
   def to_pig_latin(string)
     string.split.collect{|word| piglatinize(word)}.join(" ")
